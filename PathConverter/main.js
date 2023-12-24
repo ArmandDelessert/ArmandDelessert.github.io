3717e9b29b0ee9c39a64041c3af2f550f9ab5f89
Armand Delessert <armand.delessert@gmail.com>
2023-12-24 21:27:51 +0100

    Adds an URL parameter 'path'

--- PathConverter/main.js
+++ PathConverter/main.js
@@ -1,3 +1,12 @@
+function loadPathFromURL() {
+    var urlParams = new URLSearchParams(window.location.search);
+    var path = urlParams.get('path');
+    if (path) {
+        document.getElementById('inputPath').value = decodeURIComponent(path);
+        convertPath();
+    }
+}
+
 function convertPath() {
     var path = document.getElementById('inputPath').value;
     var uri = path.replace(/\\/g, "/").replace(/^([a-zA-Z]):/, 'file:///$1:');
